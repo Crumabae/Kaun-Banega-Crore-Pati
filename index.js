b6b9287eb05cbaae5bed01fe956d9f0a7f708596
Crumabae <61695494+Crumabae@users.noreply.github.com>
2020-03-02 19:12:18 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,127 @@
+function wrong1(){
+	document.getElementById('demo').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right1(){
+	document.getElementById('demo').innerHTML = "You Won Rs.1,000"
+}
+//Qn2
+function wrong2(){
+	document.getElementById('demoa').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right2(){
+	document.getElementById('demoa').innerHTML = "You Won Rs.2,000"
+}
+//Qn3
+function wrong3(){
+	document.getElementById('demob').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right3(){
+	document.getElementById('demob').innerHTML = "You Won Rs.3,000"
+}
+//Qn4
+function wrong4(){
+	document.getElementById('democ').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right4(){
+	document.getElementById('democ').innerHTML = "You Won Rs.5,000"
+}
+//Qn5
+function wrong5(){
+	document.getElementById('demod').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right5(){
+	document.getElementById('demod').innerHTML = "You Won Rs.10,000"
+}
+//Qn6
+function wrong6(){
+	document.getElementById('demoe').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right6(){
+	document.getElementById('demoe').innerHTML = "You Won Rs.20,000"
+}
+//Qn7
+function wrong7(){
+	document.getElementById('demof').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right7(){
+	document.getElementById('demof').innerHTML = "You Won Rs.40,000"
+}
+//Qn8
+function wrong8(){
+	document.getElementById('demog').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right8(){
+	document.getElementById('demog').innerHTML = "You Won Rs.80,000"
+}
+//Qn9
+function wrong9(){
+	document.getElementById('demoh').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right9(){
+	document.getElementById('demoh').innerHTML = "You Won Rs.1,00,000"
+}
+//Qn10
+function wrong10(){
+	document.getElementById('demoi').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right10(){
+	document.getElementById('demoi').innerHTML = "You Won Rs.3,20,000"
+}
+//Qn11
+function wrong11(){
+	document.getElementById('demoj').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right11(){
+	document.getElementById('demoj').innerHTML = "You Won Rs.6,40,000"
+}
+//Qn12
+function wrong12(){
+	document.getElementById('demok').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right12(){
+	document.getElementById('demok').innerHTML = "You Won Rs.12,50,000"
+}
+//Qn13
+function wrong13(){
+	document.getElementById('demol').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right13(){
+	document.getElementById('demol').innerHTML = "You Won Rs.25,00,000"
+}
+//Qn14
+function wrong14(){
+	document.getElementById('demom').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right14(){
+	document.getElementById('demom').innerHTML = "You Won Rs.50,00,000"
+}
+//Qn15
+function wrong15(){
+	document.getElementById('demon').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right15(){
+	document.getElementById('demon').innerHTML = "You Won 1 crore"
+}
+//Qn16
+function wrong16(){
+	document.getElementById('demoo').innerHTML = "Sorry, your answer is incorrect"
+}
+
+function right16(){
+	document.getElementById('demoo').innerHTML = "You Won 7 crore"
+}
